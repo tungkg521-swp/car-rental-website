f80eec762c83981b2abd1a1836d011673e9466ab
thanhtrung-web <tranthanhtrung120605@gmail.com>
2026-02-27 20:12:13 +0700

    View list cars

---
+++ src/main/java/Service/CarService.java
@@ -0,0 +1,25 @@
+package service;
+
+import DALs.CarDAO;
+import DALs.CarDAO;
+import models.CarModel;
+import java.util.List;
+import models.CarModel;
+
+public class CarService {
+
+    private CarDAO carDAO = new CarDAO();
+
+    public List<CarModel> findAllAvailableCars() {
+        return carDAO.findAllAvailableCars();
+    }
+    
+    public CarModel getCarById(int carId) {
+        return carDAO.findById(carId);
+    }
+    
+    public List<CarModel> findAllCars() {
+    return carDAO.findAllCars();
+}
+
+}
